694f96bb17bb1cc3e31e59a80835e88712d3d55e
benjaminr177 <unoceptik@gmail.com>
2017-11-29 14:44:23 +0200

    Flowdock token environment variable

--- flowdock.js
+++ flowdock.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch')
 var send = function(name, lasterrortime) {
   var date = new Date()
   var timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-  var flowdock_token = "5ebe97cf9ae259befb54bceb627e05f9"
+  // var flowdock_token = "5ebe97cf9ae259befb54bceb627e05f9"
+  var flowdock_token = process.env.FLOWDOCK_TOKEN
   var randomThreadId = Math.floor(Math.random() * 10000) + 1
 
   var data = {
