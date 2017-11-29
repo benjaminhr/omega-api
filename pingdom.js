b9a133c7659a56c48c931ed593f25b0525f7e680
benjaminr177 <unoceptik@gmail.com>
2017-11-29 10:37:35 +0200

    flowdock integration finally worked

--- pingdom.js
+++ pingdom.js
@@ -30,3 +30,39 @@ function ping() {
 
 // because js doesn't have sleep()
 setInterval(() => ping(), 5000)
+
+function flowdock(name) {
+  var timestamp = new Date.now().toUTCString()
+  var flowdock_token = "5ebe97cf9ae259befb54bceb627e05f9"
+
+  var data = {
+    "flow_token": `${flowdock_token}`,
+    "event": "activity",
+    "author": {
+      "name": `${name}`,
+      "avatar": "https://lh3.googleusercontent.com/BJnNSzRfzXS_hSYposOL5trRgupgQo4aP01JcoHLuBmqKY1aOgfiLpdWzDf6TzRphg=w300"
+    },
+    "title": "is down",
+    "external_thread_id": `${timestamp}`,
+    "thread": {
+      "title": `${name}`,
+      "body": "Fix it pls",
+      "external_url": "https://my.pingdom.com/newchecks/checks",
+      "status": {
+        "color": "red",
+        "value": "down"
+      }
+    }
+  }
+
+  fetch('https://api.flowdock.com/messages', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(data => data.json())
+  .then(json => console.log(json))
+}
